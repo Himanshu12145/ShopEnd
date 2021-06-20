d4e43212c26b2c7af9d6c3cd1deabb0be68ebf30
Himanshu12145 <himansu12145@gmail.com>
2021-06-20 23:25:15 +0530

    just started the express part only

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running  in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+);
